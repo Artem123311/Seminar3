a321d76941971f4966d49bc9dfe7041e8e55f4d4
Artem <mail@example.com>
2022-08-19 00:26:10 +0300

    Reverse massive

---
+++ Project01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace Massive
+{
+    class Program
+    {
+        static private void Main ()
+        {
+            Console.WriteLine("Введите числа: ");
+            var s = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            s = Enumerable.Range(0,s.Length).Select(i=>s[i]>0?-s[i]:s[i]).ToArray();
+            Array.ForEach(s,x=>Console.WriteLine($"{x}"));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
