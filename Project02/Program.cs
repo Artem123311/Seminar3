a68d4a8c4c4adb9e93ff53732c4fa4f1e25817b6
Artem <mail@example.com>
2022-08-22 03:48:17 +0300

    Сумма нечетных чисел массива

---
+++ Project02/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] a = new int[10];
+for (int i = 0; i < a.Length; i++)
+{
+    Console.Write($"Введите a[{i}] = ");
+    a[i] = int.Parse(Console.ReadLine());
+}
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] % 2 == 1)
+        sum += a[i];
+}
+Console.WriteLine($"Сумма нечетных чисел = {sum}");
